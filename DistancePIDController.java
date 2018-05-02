7039f6c730ff095493c7e64298bd29d16dc8da80
James <jawalters1@sheffield.ac.uk>
2018-05-02 14:54:54 +0100

    Controller Update.

--- DistancePIDController.java
+++ DistancePIDController.java
@@ -7,6 +7,8 @@ public class DistancePIDController {
     private static Motor leftMotor = robot.getLargeMotor(Motor.Port.C);
     private static Motor rightMotor = robot.getLargeMotor(Motor.Port.D);
     private static UltrasonicSensor sensorR = robot.getUltrasonicSensor(Sensor.Port.S1);
+    private static TouchSensor touch = robot.getTouchSensor(Sensor.Port.S4);
+    private static UltrasonicSensor sensorL = robot.getUltrasonicSensor(Sensor.Port.S2);
 
     private static float Kp = 1f;
     private static float Ki = 0;
@@ -84,17 +86,30 @@ public class DistancePIDController {
       float target = 20f;
       int speed = 75; // This is a guess to be changed.
       while (true) {
-        float value = sensorR.getDistance()*100;
-        System.out.println(value);
+        checkClose();
+        float value = sensorL.getDistance()*100;
         float error = value - target;
+        System.out.println("-------------------------------------------------");
         if (value > 125) {
+          System.out.println("error");
           error = lastError;
         }
         integral = integral + error;
         derivative = error - lastError;
         float signal = Kp*error + Ki*integral + Kd*derivative;
+        System.out.println(value);
+        System.out.println(signal);
         setMotorsTurn(speed,(int) signal);
         lastError = error;
+      }
+    }
+
+    private static void checkClose() {
+      if (touch.isTouched()) {
+        leftMotor.stop();
+        rightMotor.stop();
+        robot.close();
+        System.exit(0);
       }
     }
 
@@ -112,55 +127,64 @@ public class DistancePIDController {
         }
     }
 
-    // private static UltrasonicSensor sensorL = robot.getUltrasonicSensor(Sensor.Port.S4);
-    // private static void pidMaze() {
-    //   float integral = 0;
-    //   float lastError = 0;
-    //   float derivative = 0;
-    //   float target = 20f;
-    //   int speed = 50; // To be altered
-    //   float errorDistance = 30f; // To be altered
-    //   while (true) {
-    //     float valueR = sensorR.getDistance()*100;
-    //     float valueL = sensorL.getDistance()*100;
-    //
-    //     while (valueR > target+errorDistance) {
-    //       float value = sensorL.getDistance()*100;
-    //       float error = value - target;
-    //       integral = integral + error;
-    //       derivative = error - lastError;
-    //       float signal = Kp*error + Ki*integral + Kd*derivative;
-    //       System.out.println(speed+(-signal));
-    //       setMotorsTurn(speed,(int) signal);
-    //       lastError = error;
-    //       valueR = sensorR.getDistance()*100;
-    //     }
-    //
-    //     while (valueL > target+errorDistance) {
-    //       float value = sensorR.getDistance()*100;
-    //       float error = value - target;
-    //       integral = integral + error;
-    //       derivative = error - lastError;
-    //       float signal = Kp*error + Ki*integral + Kd*derivative;
-    //       setMotorsTurn(speed,(int) (-signal));
-    //       lastError = error;
-    //       valueL = sensorL.getDistance()*100;
-    //     }
-    //
-    //     float error = valueL-valueR;
-    //     integral = integral + error;
-    //     derivative = error - lastError;
-    //     float signal = Kp*error + Ki*integral + Kd*derivative;
-    //     System.out.println(speed+(-signal));
-    //     setMotorsTurn(speed,(int) signal);
-    //     lastError = error;
-    //     target = ((valueR+valueL)/2.0f);
-    //   }
-    // }
+    private static void pidMaze() {
+      float integral = 0;
+      float lastError = 0;
+      float derivative = 0;
+      float target = 100f;
+      int speed = 75; // To be altered
+      float errorDistance = 10f; // To be altered
+      while (true) {
+        checkClose();
+        float valueR = sensorR.getDistance()*100;
+        float valueL = sensorL.getDistance()*100;
+        System.out.println("-------------------------------------------------");
+        System.out.println(valueR);
+        System.out.println(valueL);
+
+        // while (valueR > target+errorDistance) {
+        //   checkClose();
+        //   float value = sensorL.getDistance()*100;
+        //   float error = value - target;
+        //   integral = integral + error;
+        //   derivative = error - lastError;
+        //   float signal = Kp*error + Ki*integral + Kd*derivative;
+        //   System.out.println(speed+(-signal));
+        //   setMotorsTurn(speed,(int) signal);
+        //   lastError = error;
+        //   valueR = sensorR.getDistance()*100;
+        // }
+        //
+        // while (valueL > target+errorDistance) {
+        //   checkClose();
+        //   float value = sensorR.getDistance()*100;
+        //   float error = value - target;
+        //   integral = integral + error;
+        //   derivative = error - lastError;
+        //   float signal = Kp*error + Ki*integral + Kd*derivative;
+        //   setMotorsTurn(speed,(int) (-signal));
+        //   lastError = error;
+        //   valueL = sensorL.getDistance()*100;
+        // }
+
+        float error = valueL-valueR;
+        if (valueR > 60 || valueL > 60) {
+          System.out.println("error");
+          error = lastError;
+        }
+        integral = integral + error;
+        derivative = error - lastError;
+        float signal = Kp*error + Ki*integral + Kd*derivative;
+        System.out.println(signal);
+        setMotorsTurn(speed,(int) signal);
+        lastError = error;
+        target = ((valueR+valueL)/2.0f);
+      }
+    }
 
     private static void setMotorsTurn(int speed, int steer) {
-      Int rightSpeed = speed-steer;
-      Int leftSpeed = speed+steer;
+      int rightSpeed = speed+steer;
+      int leftSpeed = speed-steer;
       if (rightSpeed < 0) {
         rightMotor.setSpeed(-rightSpeed);
         leftMotor.setSpeed(leftSpeed);
@@ -171,7 +195,7 @@ public class DistancePIDController {
         leftMotor.setSpeed(-leftSpeed);
         rightMotor.forward();
         leftMotor.backward();
-      } else
+      } else {
         rightMotor.setSpeed(rightSpeed);
         leftMotor.setSpeed(leftSpeed);
         rightMotor.forward();
@@ -180,7 +204,7 @@ public class DistancePIDController {
   }
 
     public static void main(String[] args) {
-        pid3();
+        pidMaze();
 
         // Runtime.getRuntime().addShutdownHook(new Thread() {
         //   public void run() {
